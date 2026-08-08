1ff92cbc1519d4500b59a8bf141c5d3c46c2dff2
Lihanda <fabiuslihandaachevi@gmail.com>
2026-08-08 11:29:27 +0100

    add state locking to remote state configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,7 @@ terraform {
     bucket         = "fabius-lihanda-s3-bucket"
     key            = "java-app/state.tfstate"
     region         = "eu-north-1" # region should be the same across so multiple developers can work on the same state file
+    use_lockfile     = true # state to prevent concurrent modifications
   }
 }
 
