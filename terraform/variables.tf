b995cd4f9f976207b077940a616dc4e5ee810925
Lihanda <fabiuslihandaachevi@gmail.com>
2026-08-10 21:11:09 +0100

    add .tfvars as default values

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,16 @@
 variable "vpc_cidr_block" {
-  type = string
+  type    = string
+  default = "10.0.0.0/16"
 }
 
 variable "private_subnet_cidr_blocks" {
-  type = list(string)
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "public_subnet_cidr_blocks" {
-  type = list(string)
+  type    = list(string)
+  default = ["10.0.10.0/24", "10.0.11.0/24", "10.0.12.0/24"]
 }
 
 variable "environment" {
